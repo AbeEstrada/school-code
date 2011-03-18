128d995227b1fe4433a94b7bc40ba0aaafdf3da8
Abraham Estrada <abraham.estrada@gmail.com>
2011-03-18 13:06:21 -0700

    better description

--- CM-425/blackbox.py
+++ CM-425/blackbox.py
@@ -1,6 +1,12 @@
 import sys
 
 def main():
+    """Black-box or functional testing is one in which test 
+    conditions are developed based on the program or system’s 
+    functionality, i.e., the tester re- quires information 
+    about the input data and observed output, but does not know 
+    how the program or system works. """
+    
     arg = sys.argv
     if arg[1].isdigit():
         number = int(arg[1])
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Description
 ===========
-This is code that I write for my school subjects
+Pieces of code that I write for my school subjects
 
 * SC-134 - Compilers and Interpreters
 * CM-425 - Software Testing
