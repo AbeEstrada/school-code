2661934b89da4353405a7a3a33b2be89dce6ea7f
Abraham Estrada <abraham.estrada@gmail.com>
2011-05-16 20:27:52 -0600

    new keywords

--- SC-134/lex/keywords.h
+++ SC-134/lex/keywords.h
@@ -3,7 +3,11 @@
 std::string keywords[] = {
     "void",
     "function",
+    "cin",
     "cout",
+    "endl",
+    "while",
+    "if",
     "return"
 };
 size_t keywords_size = sizeof(keywords) / sizeof(std::string);
