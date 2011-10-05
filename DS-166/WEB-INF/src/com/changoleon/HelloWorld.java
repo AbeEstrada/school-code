236024372ec1db6e0c8b07dc0a9160a551bd0d4b
Abraham Estrada <abraham.estrada@gmail.com>
2011-10-05 15:14:12 -0600

    removing comments

--- DS-166/WEB-INF/src/com/changoleon/HelloWorld.java
+++ DS-166/WEB-INF/src/com/changoleon/HelloWorld.java
@@ -6,13 +6,10 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class HelloWorld extends HttpServlet {
-    //int contador = 0;
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
         out.println("<h1>Hello</h1>");
         out.println("<p>"+new Date().toString()+"</p>");
-        //contador = contador + 1 ;
-        //out.println("<p>"+contador+"</p>");
     }
 }
